6e9cf06c4ab592510d388753ed45b387ffd38a3b
prakao <kpkrisrin@gmail.com>
2026-01-01 21:30:42 +0700

    lab11_1_finished

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,29 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+
+    cout<<"Press Enter 3 times to reveal your future.\n";
+    int i=0;
+    while(i<3){
+        cin.get();
+        i++;
+    }
+    srand(time(0));
+    int grade = rand()%9+1;
+    string x;
+    if(grade==1) x ="A";
+    else if(grade == 2)x ="B+";
+    else if(grade == 3)x ="B";
+    else if(grade == 4)x ="C+";
+    else if(grade == 5)x ="C";
+    else if(grade == 6)x ="D+";
+    else if(grade == 7)x ="D";
+    else if(grade == 8)x ="F";
+    else  x="W";
+    cout<<"You will get "<<x<<" in this 261102.";
+}   
